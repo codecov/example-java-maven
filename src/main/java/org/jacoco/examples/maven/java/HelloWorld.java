d4086b51250e5590bcb784c6c9198ba2e56265ab
julianalgcora <juliana.zaccantte@gmail.com>
2022-02-16 09:49:52 -0300

    Alerta do SourceGraph

--- src/main/java/org/jacoco/examples/maven/java/HelloWorld.java
+++ src/main/java/org/jacoco/examples/maven/java/HelloWorld.java
@@ -1,9 +1,12 @@
 package org.jacoco.examples.maven.java;
 
+import java.io.*;
+
 public class HelloWorld {
 	
 	public String getMessage(boolean bigger) {
 		if (bigger) {
+			System.out.println(bigger);
 			return "Hello Universe!";
 		} else {
 			return "Hello World!";
