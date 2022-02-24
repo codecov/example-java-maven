dd4bb2bf8bad8b3bd190a330eea25418df8df71f
Juliana Corá <juliana.cora@portoseguro.com.br>
2022-02-24 10:27:25 -0300

    Remoção do system

--- src/main/java/org/jacoco/examples/maven/java/HelloWorld.java
+++ src/main/java/org/jacoco/examples/maven/java/HelloWorld.java
@@ -6,7 +6,6 @@ public class HelloWorld {
 	
 	public String getMessage(boolean bigger) {
 		if (bigger) {
-			System.out.println("teste");
 			return "Hello Universe!";
 		} else {
 			return "Hello World!";
